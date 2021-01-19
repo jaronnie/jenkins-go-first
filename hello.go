367a6c8f7cedf853e52a9592ff78c305bac51374
cloudcoder <nj788229@gmail.com>
2021-01-19 13:22:04 +0800

    upgrade hello.go

--- hello.go
+++ hello.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-
-	"fmt"
-
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-
-	fmt.Println("hello world")
-	fmt.Println("change")
-	fmt.Println("jenkins niubi")
-
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8086") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
