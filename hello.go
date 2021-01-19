4ebd1a55be684f176f158d36d3c8d35ec33ff4f2
cloudcoder <nj788229@gmail.com>
2021-01-19 20:58:02 +0800

    change hello.go

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ponghh",
 		})
 	})
 	r.Run(":9000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
